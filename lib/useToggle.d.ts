8b04916bc503884680442822eb7ed45a0594c20b
이도현 <ksmfou98@naver.com>
2022-04-13 19:54:25 +0900

    chore: .gitignore 추가

--- lib/useToggle.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference types="react" />
-export default function useToggle(initialValue?: boolean): readonly [boolean, () => void, import("react").Dispatch<import("react").SetStateAction<boolean>>];
