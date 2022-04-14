16fac23fcfe9d00b8d8338cd84ab5edaa196b2d5
이도현 <ksmfou98@naver.com>
2022-04-14 21:28:10 +0900

    fix: hooks export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { default as useToggle } from "./useToggle";
 export { default as useInput } from "./useInput";
+export { default as useForm } from "./useForm";
+export { default as useOutsideClick } from "./useOutsideClick";
