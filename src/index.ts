434917c839306203fa8747442dc985714af5314b
이도현 <ksmfou98@naver.com>
2022-04-18 19:58:52 +0900

    fix: useDragscroll export

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as useInput } from "./useInput";
 export { default as useForm } from "./useForm";
 export { default as useOutsideClick } from "./useOutsideClick";
 export { default as useFullScreen } from "./useFullScreen";
+export { default as useDragScroll } from "./useDragScroll";
