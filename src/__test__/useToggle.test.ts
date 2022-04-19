7daebd73c0ac0cc16621b2ed6fe19dac5be4c9bc
이도현 <ksmfou98@naver.com>
2022-04-19 21:55:34 +0900

    test: useToggle returns a tuple of three values

---
+++ src/__test__/useToggle.test.ts
@@ -0,0 +1,12 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { useToggle } from "..";
+
+describe("useToggle", () => {
+  test("useToggle returns a tuple of three values", () => {
+    const { result } = renderHook(() => useToggle(false));
+    const [value, onToggle, setValue] = result.current;
+    expect(value).toBe(false);
+    expect(onToggle).toBeInstanceOf(Function);
+    expect(setValue).toBeInstanceOf(Function);
+  });
+});
