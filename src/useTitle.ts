c9b9d930462333a5852d7be77739ccb1b64f18c7
이지호 <cctv1998@naver.com>
2022-04-28 13:52:15 +0900

    refactor: useTitle remove useState

--- src/useTitle.ts
+++ src/useTitle.ts
@@ -1,10 +1,8 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
 
-export default function useTitle(initialTitle: string) {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    document.title = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
+export default function useTitle(title: string) {
+  const onChangeTitle = useCallback(() => (document.title = title), [title]);
+  useEffect(() => {
+    onChangeTitle();
+  }, [onChangeTitle]);
 }
