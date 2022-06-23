ca856a924fd2d1fb598638d86fdd1dc20d353db2
이지호 <cctv1998@naver.com>
2022-06-23 20:40:51 +0900

    chore: add useTitle in src/index.ts

--- src/useTitle.ts
+++ src/useTitle.ts
@@ -2,6 +2,7 @@ import { useCallback, useEffect } from "react";
 
 export default function useTitle(title: string) {
   const onChangeTitle = useCallback(() => (document.title = title), [title]);
+
   useEffect(() => {
     onChangeTitle();
   }, [onChangeTitle]);
